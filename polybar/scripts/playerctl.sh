f293f59a5df1465fe7dee432aaa6e0dfc1d077cd
Luna Aphelion <luna-aphelion@proton.me>
2023-01-30 16:10:06 +0000

    removed that stupid bit of code that yote everything after a dash making it so im listening to whats my age again by fucking blink

--- polybar/scripts/playerctl.sh
+++ polybar/scripts/playerctl.sh
@@ -35,8 +35,6 @@ else
         artist_trunc=$album_trunc
     fi
 
-    artist_proper=$(echo $artist_trunc | sed 's/-.*//') #this is just done becase sometimes its like "paramore - topic" when listening to songs on yt
-
     # Bar text
     #echo "$artist_trunc - $title_trunc"
     echo "$title_trunc - $artist_proper"
